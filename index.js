038fe1185a83a6daba92f4ff6366698e0bb4f4cd
TJ Holowaychuk <tj@vision-media.ca>
2013-08-28 21:17:02 -0700

    tweak output

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 
 /**
+ * Module dependencies.
+ */
+
+var debug = require('debug')('koa-compose');
+
+/**
+ * Expose compositor.
+ */
+
+module.exports = debug.enabled
+  ? instrumented
+  : compose;
+
+/**
  * Compose `middleware` returning
  * a fully valid middleware comprised
  * of all those which are passed.
@@ -9,7 +23,7 @@
  * @api public
  */
 
-module.exports = function(middleware){
+function compose(middleware){
   return function(next){
     var i = middleware.length;
     var curr;
@@ -24,4 +38,48 @@ module.exports = function(middleware){
 
     return prev;
   }
-};
+}
+
+/**
+ * Compose `middleware` instrumented for debugging.
+ *
+ * TODO: remove and support arbitrary hook?
+ *
+ * @param {Array} middleware
+ * @return {Function}
+ * @api public
+ */
+
+function instrumented(middleware){
+  return function(next){
+    var i = middleware.length;
+    var curr;
+
+    function *prev(){
+      yield next;
+    }
+
+    while (curr = middleware[--i]) {
+      prev = instrument(curr(prev));
+    }
+
+    return prev;
+  }
+}
+
+/**
+ * Instrument a middleware function.
+ *
+ * @param {Function} next
+ * @return {GeneratorFunction}
+ * @api private
+ */
+
+function instrument(next) {
+  return function *wrap(){
+    var name = next.name || '-';
+    debug('\033[33m->\033[0m %s', name);
+    yield next;
+    debug('\033[32m<-\033[0m %s', name);
+  }
+}
