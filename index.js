2a7e00c040f385577c29bce091282b426603d8b9
TJ Holowaychuk <tj@vision-media.ca>
2013-12-22 08:51:31 -0800

    add debugging support

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 
 /**
+ * Module dependencies.
+ */
+
+var debug = require('debug')('koa-compose');
+var fmt = require('util').inspect;
+
+/**
  * Expose compositor.
  */
 
-module.exports = compose;
+module.exports = debug.enabled
+  ? instrumented
+  : compose;
 
 /**
  * Compose `middleware` returning
@@ -28,6 +37,69 @@ function compose(middleware){
 
     yield *prev;
   }
+}
+
+/**
+ * Compose `middleware` returning
+ * an instrumented set of middleware
+ * for debugging manipulation between
+ * continuation.
+ *
+ * @param {Array} middleware
+ * @return {Function}
+ * @api public
+ */
+
+function instrumented(middleware){
+  console.warn('Warning: do not run DEBUG=koa-compose in production');
+  console.warn('as it will greatly affect the performance of your');
+  console.warn('application - it is designed for a development');
+  console.warn('environment only.\n');
+
+  return function *(next){
+    var i = middleware.length;
+    var prev = next || noop();
+    var name = prev.name || 'noop';
+    var curr;
+
+    while (i--) {
+      curr = middleware[i];
+      prev = wrap.call(this, curr, prev, name);
+      name = curr.name;
+    }
+
+    yield *prev;
+  }
+}
+
+/**
+ * Wrap to output debugging info.
+ *
+ * @api private
+ */
+
+function wrap(curr, prev, name) {
+  return curr.call(this, function *(next){
+    if ('noop' == name) return yield next;
+    this._level = this._level || 0;
+
+    // downstream
+    console.log('  \033[1m%d | \033[0m>> \033[36m%s\033[0m', this._level, name);
+    console.log();
+    console.log(fmt(this.response, { depth: 5, colors: true }).replace(/^/gm, '  '));
+    console.log();
+
+    // yield
+    this._level++;
+    yield next;
+    this._level--;
+
+    // upstream
+    console.log('  \033[1m%d | \033[0m<< \033[36m%s\033[0m', this._level, name);
+    console.log();
+    console.log(fmt(this.response, { depth: 5, colors: true }).replace(/^/gm, '  '));
+    console.log();
+  }.call(this, prev));
 }
 
 /**
