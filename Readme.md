29613ba03252530290b4f2e7264c1919bb9d0ee7
TJ Holowaychuk <tj@vision-media.ca>
2014-04-28 21:37:55 -0700

    remove instrumentation
    will be moved to some sort of hook system

--- Readme.md
+++ Readme.md
@@ -9,20 +9,6 @@
 
   Compose the given middleware and return middleware.
 
-## Debugging
-
-  To debug the interactions between middleware, you may use
-  the __DEBUG__ environment variable, for example:
-
-```
-$ DEBUG=koa-compose node --harmony app.js
-```
-
-  When enabled this will output verbose response information and the
-  middleware names to help visualize how they interact.
-
-  ![koa middleware debugging](https://dl.dropboxusercontent.com/u/6396913/koa/Screen%20Shot%202013-12-22%20at%208.46.46%20AM.png)
-
 ## License
 
   MIT
--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-/**
- * Module dependencies.
- */
-
-var debug = require('debug')('koa-compose');
-var fmt = require('util').inspect;
 
 /**
  * Expose compositor.
  */
 
-module.exports = debug.enabled
-  ? instrumented
-  : compose;
+module.exports = compose;
 
 /**
  * Compose `middleware` returning
@@ -35,87 +27,6 @@ function compose(middleware){
     }
 
     yield *prev;
-  }
-}
-
-/**
- * Compose `middleware` returning
- * an instrumented set of middleware
- * for debugging manipulation between
- * continuation.
- *
- * @param {Array} middleware
- * @return {Function}
- * @api public
- */
-
-function instrumented(middleware){
-  console.warn('Warning: do not run DEBUG=koa-compose in production');
-  console.warn('as it will greatly affect the performance of your');
-  console.warn('application - it is designed for a development');
-  console.warn('environment only.\n');
-
-  return function *(next){
-    var i = middleware.length;
-    var prev = next || noop();
-    var name = prev._name || prev.name || 'noop';
-    var curr;
-
-    while (i--) {
-      curr = middleware[i];
-      prev = wrap.call(this, curr, prev, name);
-      name = curr._name || curr.name;
-    }
-
-    yield *prev;
-  }
-}
-
-/**
- * Wrap to output debugging info.
- *
- * @api private
- */
-
-function wrap(curr, prev, name) {
-  return curr.call(this, function *(next){
-    if ('noop' == name) return yield next;
-    this._level = this._level || 0;
-
-    // downstream
-    output(this, 'down', name);
-
-    // yield
-    this._level++;
-    yield next;
-    this._level--;
-
-    // upstream
-    output(this, 'up', name);
-  }.call(this, prev));
-}
-
-/**
- * Output debugging information.
- */
-
-function output(ctx, direction, name) {
-  direction = 'up' == direction ? '<<' : '>>';
-  console.log('  \x1B[1m%d \x1B[0m%s \x1B[36m%s\x1B[0m', ctx._level, direction, name);
-  console.log('  \x1B[90mstatus\x1B[0m: %s %s', ctx.status, ctx.response.statusString);
-  console.log('  \x1B[90mheader\x1B[0m:');
-  header(ctx);
-  console.log('  \x1B[90mbody\x1B[0m: %j', ctx.body);
-  console.log();
-}
-
-/**
- * Output header fields.
- */
-
-function header(ctx) {
-  for (var key in ctx.response.header) {
-    console.log('    \x1B[90m%s\x1B[0m: %s', key, ctx.response.header[key]);
   }
 }
 
