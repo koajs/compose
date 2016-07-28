14352a222fbd18a8616f2bc3ed0634f815f6da6c
Jonathan Ong <jonathanrichardong@gmail.com>
2016-07-27 18:46:54 -0700

    don't affect original array
    add test for calling the middleware multiple times
    
    move iteration creation
    
    lint++

--- index.js
+++ index.js
@@ -28,43 +28,41 @@ function compose (middleware) {
    * Make an iterator for middleware.
    */
 
-  middleware[Symbol.iterator] = function () {
-    const self = this
-    const length = this.length
-    let i = 0
-    let context
-    let nextFunc
+  const obj = {
+    [Symbol.iterator]: () => {
+      const { length } = middleware
+      let i = 0
+      let context
+      let nextFunc
 
-    return {
-      next (c, n) {
-        if (!context) context = c
-        if (!nextFunc) nextFunc = n
+      return {
+        next (c, n) {
+          if (!context) context = c
+          if (!nextFunc) nextFunc = n
 
-        let fn = self[i++] || nextFunc
-        let done = i > length
-        let value
-        let nextCalled = false
+          let fn = middleware[i++] || nextFunc
+          let done = i > length
+          let value
+          let nextCalled = false
 
-        if (fn) {
-          value = fn(context, () => {
-            if (nextCalled) {
-              return Promise.reject(new Error('next() called multiple times'))
-            }
-            nextCalled = true
-            return Promise.resolve().then(() => this.next().value)
-          })
-        }
+          if (fn) {
+            value = fn(context, () => {
+              if (nextCalled) {
+                return Promise.reject(new Error('next() called multiple times'))
+              }
+              nextCalled = true
+              return Promise.resolve().then(() => this.next().value)
+            })
+          }
 
-        return {
-          value,
-          done
+          return {
+            value,
+            done
+          }
         }
       }
     }
   }
-
-  // Alternative iteration.
-  const iter = middleware[Symbol.iterator]()
 
   /**
    * @param {Object} context
@@ -73,6 +71,9 @@ function compose (middleware) {
    */
 
   return function (context, next) {
+    // iteration object
+    const iter = obj[Symbol.iterator]()
+
     try {
       return Promise.resolve(iter.next(context, next).value)
     } catch (err) {
