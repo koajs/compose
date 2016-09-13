163f7c281b98f64476ad1f499c1e84ff3450cbc1
zensh <admin@zensh.com>
2016-09-14 00:09:57 +0800

    improve compose logic.

--- index.js
+++ index.js
@@ -20,29 +20,51 @@ module.exports = compose
 
 function compose (middleware) {
   if (!Array.isArray(middleware)) throw new TypeError('Middleware stack must be an array!')
+
+  const composer = new Composer(null)
+  let nextComposer = composer
   for (const fn of middleware) {
     if (typeof fn !== 'function') throw new TypeError('Middleware must be composed of functions!')
+    nextComposer = nextComposer.addNext(fn)
   }
+  // a placehold composer for lastFn
+  nextComposer.addNext(null)
 
   /**
    * @param {Object} context
+   * @param {Function} lastFn
    * @return {Promise}
    * @api public
    */
+  return function (context, lastFn) {
+    return composer.nextFn(context, lastFn || null)()
+  }
+}
 
-  return function (context, next) {
-    // last called middleware #
-    let index = -1
-    return dispatch(0)
-    function dispatch (i) {
-      if (i <= index) return Promise.reject(new Error('next() called multiple times'))
-      index = i
-      const fn = middleware[i] || next
+class Composer {
+  constructor (fn) {
+    this.fn = fn
+    this.next = null
+  }
+
+  addNext (fn) {
+    this.next = new Composer(fn)
+    return this.next
+  }
+
+  nextFn (context, lastFn) {
+    let called = false
+    let composer = this.next
+
+    return function next () {
+      if (called) return Promise.reject(new Error('next() called multiple times'))
+      called = true
+      if (!composer) return Promise.resolve()
+      // if composer.fn not exists, it is the placehold composer
+      let fn = composer.fn || lastFn
       if (!fn) return Promise.resolve()
       try {
-        return Promise.resolve(fn(context, function next () {
-          return dispatch(i + 1)
-        }))
+        return Promise.resolve(fn(context, composer.nextFn(context, lastFn)))
       } catch (err) {
         return Promise.reject(err)
       }
