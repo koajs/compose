2ef7cd6dd63779b87f46123edfe57fe05e92502a
Stephen Mathieson <me@stephenmathieson.com>
2016-10-22 09:05:10 -0700

    remove array check

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ module.exports = compose
 
 function compose (middleware) {
   middleware = flatten(arguments)
-  if (!Array.isArray(middleware)) throw new TypeError('Middleware stack must be an array!')
   for (const fn of middleware) {
     if (typeof fn !== 'function') throw new TypeError('Middleware must be composed of functions!')
   }
