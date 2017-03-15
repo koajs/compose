f4f5397894eb1c57eee06baf54348ec9c62bc8f4
jongleberry <me@jongleberry.com>
2017-03-15 07:15:26 -0700

    package: remove co

--- test/test.js
+++ test/test.js
@@ -2,7 +2,6 @@
 
 /* eslint-env mocha */
 
-const co = require('co')
 const compose = require('..')
 const assert = require('assert')
 
@@ -15,31 +14,31 @@ describe('Koa Compose', function () {
     var arr = []
     var stack = []
 
-    stack.push(function * (context, next) {
+    stack.push(async (context, next) => {
       arr.push(1)
-      yield wait(1)
-      yield next()
-      yield wait(1)
+      await wait(1)
+      await next()
+      await wait(1)
       arr.push(6)
     })
 
-    stack.push(function * (context, next) {
+    stack.push(async (context, next) => {
       arr.push(2)
-      yield wait(1)
-      yield next()
-      yield wait(1)
+      await wait(1)
+      await next()
+      await wait(1)
       arr.push(5)
     })
 
-    stack.push(function * (context, next) {
+    stack.push(async (context, next) => {
       arr.push(3)
-      yield wait(1)
-      yield next()
-      yield wait(1)
+      await wait(1)
+      await next()
+      await wait(1)
       arr.push(4)
     })
 
-    return compose(stack.map((fn) => co.wrap(fn)))({}).then(function () {
+    return compose(stack)({}).then(function () {
       arr.should.eql([1, 2, 3, 4, 5, 6])
     })
   })
@@ -47,31 +46,31 @@ describe('Koa Compose', function () {
   it('should be able to be called twice', () => {
     var stack = []
 
-    stack.push(function * (context, next) {
+    stack.push(async (context, next) => {
       context.arr.push(1)
-      yield wait(1)
-      yield next()
-      yield wait(1)
+      await wait(1)
+      await next()
+      await wait(1)
       context.arr.push(6)
     })
 
-    stack.push(function * (context, next) {
+    stack.push(async (context, next) => {
       context.arr.push(2)
-      yield wait(1)
-      yield next()
-      yield wait(1)
+      await wait(1)
+      await next()
+      await wait(1)
       context.arr.push(5)
     })
 
-    stack.push(function * (context, next) {
+    stack.push(async (context, next) => {
       context.arr.push(3)
-      yield wait(1)
-      yield next()
-      yield wait(1)
+      await wait(1)
+      await next()
+      await wait(1)
       context.arr.push(4)
     })
 
-    const fn = compose(stack.map((fn) => co.wrap(fn)))
+    const fn = compose(stack)
     const ctx1 = { arr: [] }
     const ctx2 = { arr: [] }
     const out = [1, 2, 3, 4, 5, 6]
@@ -113,12 +112,12 @@ describe('Koa Compose', function () {
     var stack = []
     var called = false
 
-    stack.push(function * (ctx, next) {
-      yield next()
+    stack.push(async (ctx, next) => {
+      await next()
       called = true
     })
 
-    return compose(stack.map(co.wrap))({}).then(function () {
+    return compose(stack)({}).then(function () {
       assert(called)
     })
   })
@@ -126,9 +125,9 @@ describe('Koa Compose', function () {
   it('should reject on errors in middleware', function () {
     var stack = []
 
-    stack.push(function * () { throw new Error() })
+    stack.push(() => { throw new Error() })
 
-    return compose(stack.map(co.wrap))({})
+    return compose(stack)({})
       .then(function () {
         throw new Error('promise was not rejected')
       })
@@ -140,11 +139,11 @@ describe('Koa Compose', function () {
   it('should work when yielding at the end of the stack with yield*', function () {
     var stack = []
 
-    stack.push(function * (ctx, next) {
-      yield next
+    stack.push(async (ctx, next) => {
+      await next
     })
 
-    compose(stack.map(co.wrap))({})
+    compose(stack)({})
   })
 
   it('should keep the context', function () {
@@ -152,33 +151,33 @@ describe('Koa Compose', function () {
 
     var stack = []
 
-    stack.push(function * (ctx2, next) {
-      yield next()
+    stack.push(async (ctx2, next) => {
+      await next()
       ctx2.should.equal(ctx)
     })
 
-    stack.push(function * (ctx2, next) {
-      yield next()
+    stack.push(async (ctx2, next) => {
+      await next()
       ctx2.should.equal(ctx)
     })
 
-    stack.push(function * (ctx2, next) {
-      yield next()
+    stack.push(async (ctx2, next) => {
+      await next()
       ctx2.should.equal(ctx)
     })
 
-    return compose(stack.map(co.wrap))(ctx)
+    return compose(stack)(ctx)
   })
 
   it('should catch downstream errors', function () {
     var arr = []
     var stack = []
 
-    stack.push(function * (ctx, next) {
+    stack.push(async (ctx, next) => {
       arr.push(1)
       try {
         arr.push(6)
-        yield next()
+        await next()
         arr.push(7)
       } catch (err) {
         arr.push(2)
@@ -186,13 +185,13 @@ describe('Koa Compose', function () {
       arr.push(3)
     })
 
-    stack.push(function * (ctx, next) {
+    stack.push(async (ctx, next) => {
       arr.push(4)
       throw new Error()
       // arr.push(5)
     })
 
-    return compose(stack.map(co.wrap))({}).then(function () {
+    return compose(stack)({}).then(function () {
       arr.should.eql([1, 6, 4, 2, 3])
     })
   })
@@ -200,9 +199,9 @@ describe('Koa Compose', function () {
   it('should compose w/ next', function () {
     var called = false
 
-    return compose([])({}, co.wrap(function * () {
+    return compose([])({}, async () => {
       called = true
-    })).then(function () {
+    }).then(function () {
       assert(called)
     })
   })
@@ -214,7 +213,7 @@ describe('Koa Compose', function () {
       throw new Error()
     })
 
-    return compose(stack.map(co.wrap))({}).then(function () {
+    return compose(stack)({}).then(function () {
       throw new Error('promise was not rejected')
     }).catch(function (e) {
       e.should.be.instanceof(Error)
@@ -245,10 +244,10 @@ describe('Koa Compose', function () {
 
   it('should throw if next() is called multiple times', function () {
     return compose([
-      co.wrap(function * (ctx, next) {
-        yield next()
-        yield next()
-      })
+      async (ctx, next) => {
+        await next()
+        await next()
+      }
     ])({}).then(() => {
       throw new Error('boom')
     }, (err) => {
@@ -281,19 +280,19 @@ describe('Koa Compose', function () {
   it('should return last return value', function () {
     var stack = []
 
-    stack.push(function * (context, next) {
-      var val = yield next()
+    stack.push(async (context, next) => {
+      var val = await next()
       val.should.equal(2)
       return 1
     })
 
-    stack.push(function * (context, next) {
-      var val = yield next()
+    stack.push(async (context, next) => {
+      var val = await next()
       val.should.equal(0)
       return 2
     })
     var next = () => 0
-    return compose(stack.map(co.wrap))({}, next).then(function (val) {
+    return compose(stack)({}, next).then(function (val) {
       val.should.equal(1)
     })
   })
