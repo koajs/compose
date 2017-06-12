469d53d1a81fe8b468a90cb9c592605a99237716
Denis Chernov <twkr@twkrsplace.ru>
2017-06-12 13:19:37 +0300

    Refactored code using reduceRight

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 'use strict'
 
 /**
+ * Helper function that checks whether passed parameter is NOT a function
+ * @param {Object} fn
+ * @return {Function}
+ * @api private
+ */
+const isNotFunction = obj => typeof obj !== 'function';
+
+/**
+ * Helper function that wraps a function in function returning promise
+ * @param {Function} fn
+ * @return {Function}
+ * @api private
+ */
+const wrapHandler = (fn, ...args) => callCounter(() => Promise.resolve(fn(...args)))
+
+/**
+ * Decorator that throws error when function was called multiple times
+ * @param {Function} fn
+ * @return {Function}
+ * @api private
+ */
+function callCounter(fn) {
+  let calls = 0;
+  return () => {
+    if (calls++) {
+      return Promise.reject(new Error('next() called multiple times'));
+    }
+    return fn();
+  }
+}
+
+/**
  * Expose compositor.
  */
-
 module.exports = compose
 
 /**
@@ -15,8 +46,30 @@ module.exports = compose
  * @return {Function}
  * @api public
  */
+function compose(middleware){
+  if (!Array.isArray(middleware)) {
+    throw new TypeError('Middleware stack must be an array!');
+  }
+  if (middleware.some(isNotFunction)) {
+    throw new TypeError('Middleware must be composed of functions!');
+  }
 
-function compose (middleware) {
+  /**
+   * @param {Object} context
+   * @return {Promise}
+   * @api public
+   */
+  return (context, next) => {
+    if (!next) {
+      next = () => Promise.resolve();
+    }
+    next = callCounter(next);
+    const chain = middleware.reduceRight((next, fn) => wrapHandler(fn, context, next), next);
+    return chain();
+  }
+}
+
+/* function compose (middleware) {
   if (!Array.isArray(middleware)) throw new TypeError('Middleware stack must be an array!')
   for (const fn of middleware) {
     if (typeof fn !== 'function') throw new TypeError('Middleware must be composed of functions!')
@@ -28,7 +81,7 @@ function compose (middleware) {
    * @api public
    */
 
-  return function (context, next) {
+/*  return function (context, next) {
     // last called middleware #
     let index = -1
     return dispatch(0)
@@ -47,4 +100,4 @@ function compose (middleware) {
       }
     }
   }
-}
+} */
