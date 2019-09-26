95433143e1384ff8ebe20cabf14c55ccfcbf0d9c
Kévin Berthommier <kevin.bertho@gmail.com>
2019-09-27 01:54:37 +0200

    conserve this

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function compose (middleware) {
   return function (context, next) {
     // last called middleware #
     let index = -1
-    return dispatch(0)
+    return dispatch.call(this, 0)
     function dispatch (i) {
       if (i <= index) return Promise.reject(new Error('next() called multiple times'))
       index = i
@@ -39,7 +39,7 @@ function compose (middleware) {
       if (i === middleware.length) fn = next
       if (!fn) return Promise.resolve()
       try {
-        return Promise.resolve(fn(context, dispatch.bind(null, i + 1)));
+        return Promise.resolve(fn.call(this, context, dispatch.bind(this, i + 1)));
       } catch (err) {
         return Promise.reject(err)
       }
