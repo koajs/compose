9ecca48a6e04e0a6a6dc41ff20a5ce153ba80c4f
3imed-jaberi <imed_jebari@hotmail.fr>
2020-07-15 13:07:12 +0200

    feat: use async/await over promise + better var. names

--- index.js
+++ index.js
@@ -17,32 +17,29 @@ module.exports = compose
  */
 
 function compose (middleware) {
-  if (!Array.isArray(middleware)) throw new TypeError('Middleware stack must be an array!')
+  if (!Array.isArray(middleware))
+    throw new TypeError('Middleware stack must be an array!')
+
   for (const fn of middleware) {
-    if (typeof fn !== 'function') throw new TypeError('Middleware must be composed of functions!')
+    if (typeof fn !== 'function')
+      throw new TypeError('Middleware must be composed of functions!')
   }
 
-  /**
-   * @param {Object} context
-   * @return {Promise}
-   * @api public
-   */
 
   return function (context, next) {
-    // last called middleware #
-    let index = -1
-    return dispatch(0)
-    function dispatch (i) {
-      if (i <= index) return Promise.reject(new Error('next() called multiple times'))
-      index = i
-      let fn = middleware[i]
-      if (i === middleware.length) fn = next
-      if (!fn) return Promise.resolve()
+    let lastCalledIndex = -1
+    return (async function dispatch (currentCallIndex) {
+      if (currentCallIndex <= lastCalledIndex)
+        throw new Error('next() called multiple times')
+      lastCalledIndex = currentCallIndex
+      let fn = middleware[currentCallIndex]
+      if (currentCallIndex === middleware.length) fn = next
+      if (!fn) return;
       try {
-        return Promise.resolve(fn(context, dispatch.bind(null, i + 1)));
+        return fn(context, dispatch.bind(null, currentCallIndex + 1));
       } catch (err) {
-        return Promise.reject(err)
+        throw err
       }
-    }
+    })(0);
   }
 }
