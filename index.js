b4d6246f04f795b0096d304e56d2078a828412c9
Anh Hao <hi@anhhao.me>
2021-01-17 23:07:29 +0700

    add inject middleware method

--- index.js
+++ index.js
@@ -16,11 +16,12 @@ module.exports = compose
  * @api public
  */
 
-function compose (middleware) {
-  if (!Array.isArray(middleware)) throw new TypeError('Middleware stack must be an array!')
-  for (const fn of middleware) {
+function compose (_middleware) {
+  if (!Array.isArray(_middleware)) throw new TypeError('Middleware stack must be an array!')
+  for (const fn of _middleware) {
     if (typeof fn !== 'function') throw new TypeError('Middleware must be composed of functions!')
   }
+
 
   /**
    * @param {Object} context
@@ -29,8 +30,16 @@ function compose (middleware) {
    */
 
   return function (context, next) {
+    let middleware = [..._middleware];
+
     // last called middleware #
     let index = -1
+
+    // inject middleware
+    const injectMiddleware = fn => {
+      middleware = [...middleware.slice(0, index + 1), fn, ...middleware.slice(index + 1, middleware.length)];
+    };
+    
     return dispatch(0)
     function dispatch (i) {
       if (i <= index) return Promise.reject(new Error('next() called multiple times'))
@@ -39,7 +48,7 @@ function compose (middleware) {
       if (i === middleware.length) fn = next
       if (!fn) return Promise.resolve()
       try {
-        return Promise.resolve(fn(context, dispatch.bind(null, i + 1)));
+        return Promise.resolve(fn(context, dispatch.bind(null, i + 1), injectMiddleware));
       } catch (err) {
         return Promise.reject(err)
       }
